c40b30606707371191223a9fdfb8dc474eff9f23
Chirag Laheru <chiraglaheru6@gmail.com>
2026-02-26 16:00:39 +0530

    Remove user factory from seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,5 @@ class DatabaseSeeder extends Seeder
         $this->call([AdminSeeder::class]);
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
     }
 }

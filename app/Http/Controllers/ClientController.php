b444b7e246b607f209014a30bfe2931b1b28edb5
Chirag <140259132+chiraglaheru@users.noreply.github.com>
2026-04-14 00:14:52 +0530

    Update ClientController.php

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Models\Client;
-use App\Models\City;
 
 class ClientController extends Controller
 {
